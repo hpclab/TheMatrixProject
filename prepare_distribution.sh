5fbfb63d2a83e1a13caafe993437294cacbbff3c
Emanuele Carlini <emanuele.carlini@isti.cnr.it>
2014-09-03 16:02:16 +0200

    fix few lines in prepare_distribution.sh

--- prepare_distribution.sh
+++ prepare_distribution.sh
@@ -15,15 +15,18 @@ ant -f build-jar.xml
 # compile the javadoc
 ant -f build-javadoc.xml
 
+# create the empty directories
+mkdir $DIR/iad/
+mkdir $DIR/results/ 
+mkdir $DIR/scripts/
+
 # copy directories to the destination directory
-cp -r ./javadoc $DIR
-cp -r ./iad $DIR
-cp -r ./lib $DIR
-cp -r ./lookups $DIR
-cp -r ./mapping $DIR
-cp -r ./queries $DIR
-cp -r ./results $DIR
-cp -r ./scripts $DIR
+cp -r ./javadoc/ $DIR
+cp -r ./lib/ $DIR
+cp -r ./lookups/ $DIR
+cp -r ./mapping/ $DIR
+cp -r ./queries/ $DIR
+
 
 # copy files to the destination directory
 cp MetaXmlCreator $DIR
@@ -33,8 +36,13 @@ cp TheMatrix $DIR
 cp TheMatrixWIN.bat $DIR
 cp TheMatrixB.jar $DIR
 
-# zip the directory
-zip TheMatrixDistribution.zip $DIR/*
+# zip the directory (delete if already present)
+ZIPFILE=$DIR.zip
+if [ -f "$ZIPFILE" ]; then
+  rm $ZIPFILE
+fi
+
+zip -rq $ZIPFILE $DIR
 
 # delete the directory
 rm -rf $DIR
