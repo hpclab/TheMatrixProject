f32ffe30a94354aa4f1926e90008f4f06dd6baa9
Emanuele Carlini <emanuele.carlini@isti.cnr.it>
2014-09-04 11:37:42 +0200

    fix MetaXmlCreator path

--- MetaXmlCreatorWIN.bat
+++ MetaXmlCreatorWIN.bat
@@ -3,4 +3,4 @@
 ::
 :: unico argomento: il nome del dataset da abilitare (senza la desinenza .csv)
 ::
-java -Xms128m -Xmx1024m -XX:MaxPermSize=256m -classpath C:\TheMatrix\TheMatrixB.jar;C:\TheMatrix\libs\Dexter.jar;C:\TheMatrix\libs\Lexter.jar;C:\TheMatrix\libs\Neverlang.jar;C:\TheMatrix\libs\commons-io-2.4.jar;C:\TheMatrix\libs\jopt-simple-4.3.jar;C:\TheMatrix\libs\junit-4.11.jar;C:\TheMatrix\libs\mysql-connector-java-5.1.18.jar;C:\TheMatrix\libs\ojdbc6-11.1.0.7.0.jar;C:\TheMatrix\libs\sqljdbc4.jar;C:\TheMatrix\libs\super-csv-2.1.0.jar it.cnr.isti.thematrix.tools.MetaXmlCreator %1
+java -Xms128m -Xmx1024m -XX:MaxPermSize=256m -classpath C:\TheMatrix\TheMatrixB.jar;C:\TheMatrix\lib\Dexter.jar;C:\TheMatrix\lib\Lexter.jar;C:\TheMatrix\lib\Neverlang.jar;C:\TheMatrix\lib\commons-io-2.4.jar;C:\TheMatrix\lib\jopt-simple-4.3.jar;C:\TheMatrix\lib\junit-4.11.jar;C:\TheMatrix\lib\mysql-connector-java-5.1.18.jar;C:\TheMatrix\lib\ojdbc6-11.1.0.7.0.jar;C:\TheMatrix\lib\sqljdbc4.jar;C:\TheMatrix\lib\super-csv-2.1.0.jar it.cnr.isti.thematrix.tools.MetaXmlCreator %1
